0557fb7f163c00275cc77e478a3f07a3327dd0ef
MikApp <miguelpaucar987@gmail.com>
2024-12-18 19:25:32 -0500

    agregado superuser

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Crear superusuario usando variables de ambiente
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"mike"}
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"miguelpaucar987@gmial.com"}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-"50sombrasdegrey"}
+
+python manage.py createsuperuser --noinput
